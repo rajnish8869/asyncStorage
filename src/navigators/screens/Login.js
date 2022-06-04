ff4306280b3d301f84130127625dcee501244710
Stuti Singhal <stutisinghal1312@gmail.com>
2022-06-05 01:16:37 +0530

    with single data feild

---
+++ src/navigators/screens/Login.js
@@ -0,0 +1,73 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as React from 'react';
+import {useState, useEffect} from 'react';
+import {
+  Button,
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+
+const Login = ({navigation}) => {
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      await AsyncStorage.getItem('UserName').then(value => {
+        if (value != null) {
+          navigation.navigate('home');
+        }
+      });
+    } catch (error) {
+      console.log('E R R O R', error);
+    }
+  };
+
+  const setData = async () => {
+    if (name.length == 0) {
+      Alert.alert('warning', 'Enter the username');
+    } else {
+      try {
+        await AsyncStorage.setItem('UserName', name);
+        navigation.navigate('home');
+      } catch (error) {
+        console.log('ERROR', error);
+      }
+    }
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'pink',
+      }}>
+      <Text style={{fontSize: 30, margin: 30}}>Async storage</Text>
+      <TextInput
+        placeholder="Enter username"
+        style={{
+          borderWidth: 2,
+          borderColor: 'red',
+          borderRadius: 10,
+          padding: 10,
+          margin: 30,
+        }}
+        onChangeText={val => setName(val)}
+      />
+      <Button title="LOGIN" onPress={setData} />
+    </View>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({});
